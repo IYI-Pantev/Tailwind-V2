4db7a21eea2cf67212a8ccbfae1509ff4a0c3ee7
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-07-23 23:20:06 +0300

    basic config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center gap-2 bg-amber-50 mt-2">
+      <p className="text-3xl font-bold text-green-500">Some text</p>
+      <p className="border-b-2 text-2xl font-extralight text-green-400 underline">
+        Other text
+      </p>
+    </div>
+  );
+}
+
+export default App;
